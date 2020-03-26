e7d7653d3d372805c5a06a4e099648eb2a379591
robin-j9 <robin@robinjun.com>
2020-03-25 19:02:11 -0500

    populated List<SelectListItem> collections in NewJobViewModel

--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -35,6 +35,8 @@ namespace TechJobs.ViewModels
         public List<SelectListItem> CoreCompetencies { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> PositionTypes { get; set; } = new List<SelectListItem>();
 
+
+
         public NewJobViewModel()
         {
 
@@ -43,6 +45,33 @@ namespace TechJobs.ViewModels
             foreach (Employer field in jobData.Employers.ToList())
             {
                 Employers.Add(new SelectListItem {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
                     Value = field.ID.ToString(),
                     Text = field.Value
                 });
